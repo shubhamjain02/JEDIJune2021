6cb8af9e4fee85102cadef1bd3edacd07a057587
shubham <jain.shubham102@gmail.com>
2021-06-17 10:12:25 +0530

    Changes to be committed: 	Added Summation of Four Int Arguments 	Refactored Java Code

--- CCode/addition.c
+++ CCode/addition.c
@@ -9,3 +9,8 @@ int sum3(int x, int y, int z)
 {
 	return x+y+z;
 }
+
+int sum4(int x, int y, int z, int a)
+{
+	return a+z+x+y;
+}
---
+++ JavaCode/HelloJava.java
@@ -0,0 +1,12 @@
+package learnJava;
+
+public class Hello {
+	public static void main(String[] args)
+	{
+		helloWorld();
+	}
+	public void helloWorld()
+	{
+		System.out.println("Hello, World!");
+	}
+}
